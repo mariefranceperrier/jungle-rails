201a7b51b2456c24ce190bbef3841ff146912885
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-05-21 16:40:30 -0400

    add home page feature spec with cypress

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('Visit Home Page', () => {
+  it('should visit the home page', () => {
+    cy.visit('/');
+  });
+
+  it("There are products on the page", () => {
+    cy.visit('/');
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
